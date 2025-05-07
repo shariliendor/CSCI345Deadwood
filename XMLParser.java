3dd35419c4438c60d3334a0870bd1519f2257470
Squareoman <jrees@engdevnet.com>
2025-05-07 16:00:53 -0700

    updated XMLParser and Deck

--- XMLParser.java
+++ XMLParser.java
@@ -8,35 +8,57 @@ import org.w3c.dom.Element;
 
 public class XMLParser {
    public Deck parseDeck() {
-      try {      	  
-	     //Input the XML file
-	     File inputXmlFile = new File("cards.xml");
-	      
-	     //creating DocumentBuilder
-	     DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	     DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
-	     Document xmldoc = docBuilder.parse(inputXmlFile);
-	      
-	     //Getting the root element
-	     System.out.println("Root element :" + xmldoc.getDocumentElement().getNodeName());
-	     NodeList nList = xmldoc.getElementsByTagName("card");
-	      
-	     //Iterating through all the child elements of the root
-	     for (int temp = 0; temp < nList.getLength(); temp++) {      
-            Node nNode = nList.item(temp);
-            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-	        
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {     
-               Element eElement = (Element) nNode;
-               System.out.println("name : " + eElement.getAttribute("name"));
-               System.out.println("img : " + eElement.getAttribute("img"));
-               System.out.println("budget : " + eElement.getAttribute("budget"));
-            }
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+       Deck deck = new Deck();
+       try {
+           File inputXmlFile = new File("cards.xml");
+           DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
+           Document xmldoc = docBuilder.parse(inputXmlFile);
+           xmldoc.getDocumentElement().normalize();
+
+           NodeList cardNodes = xmldoc.getElementsByTagName("card");
+
+           for (int i = 0; i < cardNodes.getLength(); i++) {
+               Element cardElement = (Element) cardNodes.item(i);
+
+               String name = cardElement.getAttribute("name");
+               String img = cardElement.getAttribute("img");
+               int budget = Integer.parseInt(cardElement.getAttribute("budget"));
+
+               Element sceneElement = (Element) cardElement.getElementsByTagName("scene").item(0);
+               String sceneDescription = sceneElement.getTextContent().trim();
+
+               NodeList partNodes = cardElement.getElementsByTagName("part");
+               Role[] roles = new Role[partNodes.getLength()];
+
+               for (int j = 0; j < partNodes.getLength(); j++) {
+                   Element partElement = (Element) partNodes.item(j);
+                   String roleName = partElement.getAttribute("name");
+                   int level = Integer.parseInt(partElement.getAttribute("level"));
+
+                   Element areaElement = (Element) partElement.getElementsByTagName("area").item(0);
+                   int x = Integer.parseInt(areaElement.getAttribute("x"));
+                   int y = Integer.parseInt(areaElement.getAttribute("y"));
+                   int h = Integer.parseInt(areaElement.getAttribute("h"));
+                   int w = Integer.parseInt(areaElement.getAttribute("w"));
+                   Area area = new Area(x, y, h, w);
+
+                   String line = partElement.getElementsByTagName("line").item(0).getTextContent();
+
+                   roles[j] = new Role(roleName, level, line, area);
+               }
+
+               Card card = new Card(name, img, budget, sceneDescription, roles);
+               deck.addCard(card);
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       return deck;
    }
 
-   public Board parseBoard(String fileName) {}
+   public Board parseBoard(String fileName) {
+       // Placeholder
+       return null;
+   }
 }
