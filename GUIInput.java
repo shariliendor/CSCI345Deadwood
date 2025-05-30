be0c939c17eb25a4cb2784af50991bc7e50d9189
Squareoman <jrees@engdevnet.com>
2025-05-30 14:21:42 -0700

    Update GUIInput.java

--- GUIInput.java
+++ GUIInput.java
@@ -1,22 +1,28 @@
 import javax.swing.*;
 
 public class GUIInput implements Input{
-    private JPanel interfacePanel = new JPanel();
+    private JFrame frame;
 
     public GUIInput(JFrame frame) {
-
+        this.frame = frame;
     }
     @Override
     public int getNumPlayers() {
-        // text input box
-        // validate number between 2 and 8
-        return 0;
+        while (true) {
+            String input = JOptionPane.showInputDialog(frame, "Enter the number of players (2-8):");
+            try {
+                int num = Integer.parseInt(input);
+                if (num >= 2 && num <= 8) {
+                    return num;
+                }
+            } catch (NumberFormatException ignored) {}
+            JOptionPane.showMessageDialog(frame, "Invalid input. Enter a number between 2 and 8.");
+        }
     }
 
     @Override
     public String getPlayerName(int playerNum) {
-        // text input box
-        return null;
+        return JOptionPane.showInputDialog(frame, "Enter name for player " + playerNum + ":");
     }
 
     @Override
